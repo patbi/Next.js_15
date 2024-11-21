a41a12cb879b477b7d42e03cb0a4cfa5d899b7c8
Patrick <biyagapatrick@gmail.com>
2024-11-22 00:01:26 +0100

    Navigation

--- src/app/components/navigation.tsx
+++ src/app/components/navigation.tsx
@@ -1,6 +1,11 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export const Navigation = () => {
+	const pathname = usePathname();
+
 	return (
 		<nav>
 			<Link href="/" className="mr-4 text-blue-500">
